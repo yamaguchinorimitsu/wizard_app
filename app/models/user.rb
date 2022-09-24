0b8523bd918f8ecf4a7b93679b9445dfae20e66a
yamaguchinorimitsu <yamaguchi.nori1001@gmail.com>
2022-09-24 22:24:23 +0900

    ウイザード式

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :name, :age ,presence: true
+  has_one :address
+end
+
